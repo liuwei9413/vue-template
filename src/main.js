f1d79e4436da9549e2927ff529e906806457cf72
Asbert Ma <asbert.ma@vechain.com>
2017-04-14 14:19:05 +0800

    Add js-cookies

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import 'normalize.css'
 import '../ele-theme/index.css'
 import './css/el-overwrite.css'
 
+import 'js-cookie'
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import router from './router'
@@ -19,6 +20,9 @@ Vue.config.productionTip = false
 
 // set the API root so we can use relative url's in our actions.
 Vue.http.options.root = 'http://localhost:3000'
+
+// Vue.http.headers.common['Authorization'] = 'asdf';
+// Vue.http.headers.common['Authorization'] = window.Cookies.get('token');
 
 /* eslint-disable no-new */
 window.MPApp = new Vue({
